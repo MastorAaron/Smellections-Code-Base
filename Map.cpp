0fc53e936b30aed417539a737bf6e19961d8e3bd
Rieser, Aaron <aaronrieser@my.unt.edu>
2025-06-10 20:56:58 -0500

    Finished formatting

--- Map.cpp
+++ Map.cpp
@@ -1,44 +1,38 @@
 #include "Map.h"
-using std::string;
+using namespace std;
 
 Map::Map()
 	: m_keys(new string[INITIAL_CAPACITY]),
 	m_values (new string[INITIAL_CAPACITY]),
 	m_capacity(INITIAL_CAPACITY),
 	m_size(0),
-	m_readOnly(false) 
-{
+	m_readOnly(false){
 }
-Map::~Map() 
-{
+
+Map::~Map(){
 	delete[] m_keys;
 	delete[] m_values;
 }
-bool Map::isEmpty() 
-{
+
+bool Map::isEmpty(){
 	return m_size == 0;
 }
+
 // Do nothing because user must input key and value
-void Map::add(string element) 
-{
+void Map::add(string element){
 }
-void Map::add(string key, string value) 
-{
-	if (!m_readOnly) 
-	{
-		for (int i = 0; i < m_size; i++)
-			if (m_keys[i] == key) 
-			{
+
+void Map::add(string key, string value){
+	if (!m_readOnly){
+		for (int i = 0; i < m_size; i++){
+			if (m_keys[i] == key){
 				m_values[i] = value;
 				return;
 			}
 		int newSize = m_size + 1;
-		if (newSize > m_capacity) 
-		{
-			string* newKeys = 
-				new string[m_capacity + INITIAL_CAPACITY];
-			string* newValues = 
-				new string[m_capacity + INITIAL_CAPACITY];
+		if (newSize > m_capacity){
+			string* newKeys   = new string[m_capacity + INITIAL_CAPACITY];
+			string* newValues = new string[m_capacity + INITIAL_CAPACITY];
 			m_capacity += INITIAL_CAPACITY;
 			memcpy(newKeys, m_keys, sizeof(m_keys));
 			memcpy(newValues, m_values, sizeof(m_values));
@@ -50,55 +44,57 @@ void Map::add(string key, string value)
 		m_keys[m_size] = key;
 		m_values[m_size] = value;
 		m_size++;
+		}
 	}
 }
-int Map::getSize() 
-{
+
+int Map::getSize(){
 	return m_size;
 }
-bool Map::remove(string key) 
-{
+
+bool Map::remove(string key){
 	if (m_readOnly)
 		return false;
-	for (int i = 0; i < m_size; i++)
-		if (m_keys[i] == key) 
-		{
+
+	for (int i = 0; i < m_size; i++){
+		if (m_keys[i] == key){
 			m_keys[i] = "";
 			m_values[i] = "";
 			m_size--;
 			return true;
 		}
+	}
 	return false;
 }
-bool Map::contains(string value) 
-{
+
+bool Map::contains(string value){
 	for (int i = 0; i < m_size; i++)
 		if ((value == "" && m_values[i] == "")
 			|| (m_values[i] != "" && m_values[i] == value))
 			return true;
 	return false;
 }
-bool Map::containsKey(string key) 
-{
-	for (int i = 0; i < m_size; i++)
-		if (m_keys[i] != "" && m_keys[i] == key) 
-		{
+
+bool Map::containsKey(string key){
+	for (int i = 0; i < m_size; i++){
+		if (m_keys[i] != "" && m_keys[i] == key){
 			m_indexWhereKeyFound = i;
 			return true;
 		}
+	}
 	return false;
 }
-string Map::get(string key) 
-{
+
+string Map::get(string key){
 	if (!containsKey(key))
 		return "";
 	return m_values[m_indexWhereKeyFound];
 }
-int Map::getCapacity() 
-{
+
+int Map::getCapacity(){
 	return m_capacity;
 }
-void Map::setReadOnly(bool b) 
-{
+
+void Map::setReadOnly(bool b){
 	m_readOnly = b;
 }
--- Set.cpp
+++ Set.cpp
@@ -6,94 +6,90 @@ Set::Set()
 	: m_capacity(INITIAL_CAPACITY),
 	m_elements(new string[INITIAL_CAPACITY]),
 	m_size(0),
-	m_readOnly(false) 
-{
+	m_readOnly(false){
 }
-Set::~Set() 
-{
+
+Set::~Set(){
 	delete[] m_elements;
 }
-bool Set::isEmpty() 
-{
+
+bool Set::isEmpty(){
 	return m_size == 0;
 }
-void Set::add(string element) 
-{
-	if (!m_readOnly) 
-	{
+
+void Set::add(string element){
+	if(!m_readOnly){
+		if(contains(element))
+			return;
+
 		int newSize = m_size + 1;
-		if (newSize > m_capacity) 
-		{
+		if(newSize > m_capacity){
 			m_capacity += INITIAL_CAPACITY;
 			string* newElements = new string[m_capacity];
-			for (int i = 0; i < m_size; i++)
+			for(int i = 0; i < m_size; i++)
 				newElements[i] = m_elements[i];
 			delete[] m_elements;
 			m_elements = newElements;
 		}
-		if (contains(element))
-			return;
+		
 		m_elements[m_size++] = element;
 	}
 }
-bool Set::contains(string element) 
-{
-	for (int i = 0; i < m_size; i++)
-		if (m_elements[i] == element)
+bool Set::contains(string element){
+	for(int i = 0; i < m_size; i++)
+		if(m_elements[i] == element)
 			return true;
 	return false;
 }
-int Set::getSize() 
-{
+
+int Set::getSize(){
 	return m_size;
 }
-bool Set::remove(string element) 
-{
-	if (m_readOnly)
+
+bool Set::remove(string element){
+	if(m_readOnly)
 		return false;
-	for (int i = 0; i < m_size; i++)
-		if (m_elements[i] == element) 
-		{
+
+	for(int i = 0; i < m_size; i++){
+		if(m_elements[i] == element){
 			m_elements[i] = "";
 			m_capacity = m_size - 1;
 			string* newElements = new string[m_capacity];
 			int k = 0;
-			for (int j = 0; j < m_size; j++) 
-			{
-				if (m_elements[j] != "")
+			for(int j = 0; j < m_size; j++){
+				if(m_elements[j] != "")
 					newElements[k++] = m_elements[j];
 			}
+
 			m_size--;
 			delete[] m_elements;
 			m_elements = newElements;
 			return true;
 		}
+	}
 	return false;
 }
-string Set::getElementAt(int index) 
-{
-	if (index >= m_size) 
-	{
-		ArrayIndexOutOfBoundsException e;b
+
+string Set::getElementAt(int index){
+	if(index >= m_size){
+		ArrayIndexOutOfBoundsException e;
 		throw e;
 	}
 	return m_elements[index];
 }
-void Set::addAll(List& l) 
-{
-	for (int i=0; i < l.size(); i++) 
-	{
-		if (!contains(l.get(i))) 
-		{
+
+void Set::addAll(List& l){
+	for(int i=0; i < l.size(); i++){
+		if(!contains(l.get(i))){
 			m_elements[m_size++] = l.get(i);
 		}
 	}
 }
-int Set::getCapacity() 
-{
+
+int Set::getCapacity(){
 	return m_capacity;
 }
-void Set::setReadOnly(bool b) 
-{
+
+void Set::setReadOnly(bool b){
 	m_readOnly = b;
 }
